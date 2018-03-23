1d1e543b98dd702791d97c0b500093f4e11c2514
IsmaelJDz <Ismael_br7@hotmail.com>
2018-03-23 15:06:49 -0600

    App finished with PHP and VUE

--- index.php
+++ index.php
@@ -96,27 +96,246 @@
         <h3 class="left">Lista de estudiantes</h3>
       </div>
       <div class="col s2">
-        <button class="btn-large btn-floating">
+        <button class="btn-large btn-floating" @click="toggleModal('add')">
           <i class="material-icons">add_circle</i>
         </button>
       </div>
     </section>
     <hr>
     <transition name="fade">
-      <p class="u-flexColumnCenter red accent-1 red-text text-darken-4">
+      <p class="u-flexColumnCenter red accent-1 red-text text-darken-4"
+      v-if="errorMessage">
         {{ errorMessage }}
         <i class="material-icons prefix">error</i>
       </p>
-      <p class="u-flexColumnCenter green accent-1 green-text text-darken-4">
+      <p class="u-flexColumnCenter green accent-1 green-text text-darken-4"
+      v-if="successMessage">
         {{ successMessage }}
         <i class="material-icons prefix">check_circle</i>
       </p>
+    </transition>
+    <transition name="fade">
+      <table class="responsive-table stripe">
+      <tr>
+        <th>Id</th>
+        <th>Nombre</th>
+        <th>Email</th>
+        <th>Web</th>
+        <th>Editar</th>
+        <th>Borrar</th>
+      </tr>
+      <tr v-for="student in students" :key="student.id">
+        <td>{{ student.id }}</td>
+        <td>{{ student.name }}</td>
+        <td>{{ student.email }}</td>
+        <td>{{ student.web }}</td>
+        <td>
+          <button class="btn btn-floating" @click="getStudent('edit', student)">
+            <i class="material-icons">edit</i>
+          </button>
+        </td>
+        <td>
+          <button class="btn btn-floating" @click="getStudent('delete', student)">
+            <i class="material-icons">delete</i>
+          </button>
+        </td>
+      </tr>
+      </table>
+    </transition>
+    <transition name="fade">
+      <section :class="[ 'ModalWindow', displayAddModal ]" v-if="showAddModal">
+        <div class="ModalWindow-container">
+          <header class="ModalWindow-heading">
+            <div class="row valign-wrapper">
+              <div class="col s10">
+                <h4 class="left">Agregar Estudiante</h4>
+              </div>
+              <div class="col s2">
+                <button class="btn btn-floating right" @click="toggleModal('add')">
+                  <i class="material-icons">close</i>
+                </button>
+              </div>
+            </div>
+          </header>
+          <form class="ModalWindow-content row" @submit.prevent="createStudent">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">account_circle</i>
+              <input name="name" type="text" placeholder="Nombre" required>
+            </div>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">email</i>
+              <input name="email" type="text" placeholder="Correo" required>
+            </div>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">web</i>
+              <input name="web" type="text" placeholder="Web" required>
+            </div>
+            <div class="input-field col s12">
+              <button class="btn-large btn-floating right" type="submit">
+                <i class="material-icons">save</i>
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </transition>
+    <transition name="fade">
+      <section :class="['ModalWindow', displayEditModal]" v-if="showEditModal">
+        <div class="ModalWindow-container">
+          <header class="ModalWindow-heading">
+            <div class="row valign-wrapper">
+              <div class="col s10">
+                <h4 class="left">Editar Estudiante</h4>
+              </div>
+              <div class="col s2">
+                <button class="btn btn-floating right" @click="toggleModal('edit')">
+                  <i class="material-icons">close</i>
+                </button>
+              </div>
+            </div>
+          </header>
+          <form class="ModalWindow-content row" @submit.prevent="updateStudent">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">account_circle</i>
+              <input v-model="activeStudent.name" name="name" type="text" placeholder="Nombre" required>
+            </div>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">email</i>
+              <input v-model="activeStudent.email" name="email" type="text" placeholder="Correo" required>
+            </div>
+            <div class="input-field col s12">
+              <i class="material-icons prefix">web</i>
+              <input v-model="activeStudent.web" name="web" type="text" placeholder="Web" required>
+            </div>
+            <div class="input-field col s12">
+              <button class="btn-large btn-floating right" type="submit">
+                <i class="material-icons">save</i>
+              </button>
+              <input v-model="activeStudent.id" name="id" type="hidden" required>
+            </div>
+          </form>
+        </div>
+      </section>
+    </transition>
+    <transition name="fade">
+      <section :class="['ModalWindow', displayDeleteModal]" v-if="showDeleteModal">
+        <div class="ModalWindow-container">
+          <header class="ModalWindow-heading">
+            <div class="row valign-wrapper">
+              <div class="col s10">
+                <h4 class="left">Eliminar Estudiante</h4>
+              </div>
+              <div class="col s2">
+                <button class="btn btn-floating right" @click="toggleModal('delete')">
+                  <i class="material-icons">close</i>
+                </button>
+              </div>
+            </div>
+          </header>
+          <form class="ModalWindow-content row" @submit.prevent="deleteStudent">
+            <div class="input-field col s12">
+              <p class="flow-text center">¿Estás seguro de eliminar al estudiante: <b>{{activeStudent.name}}</b>.</p>
+              <input v-model="activeStudent.id" name="id" type="hidden" required>
+            </div>
+            <div class="input-field col s4 offset-s4">
+              <button class="btn-large btn-floating left" type="submit">
+                <i class="material-icons">check</i>
+              </button>
+              <button class="btn-large btn-floating right" type="button" @click="toggleModal('delete')">
+                <i class="material-icons">close</i>
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
     </transition>
   </main>
   <script src="https://cdn.jsdelivr.net/npm/vue"></script>
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   <script>
+    const app = new Vue({
+      el: "#app",
+      data: {
+        showAddModal: false,
+        showEditModal: false,
+        showDeleteModal: false,
+        errorMessage: '',
+        successMessage: '',
+        students: [],
+        activeStudent: {}
+      },
+      mounted(){
+        this.getAllStudents()
+      },
+      computed: {
+        displayAddModal(){
+          return (this.showAddModal==true) ? 'u-show' : ''
+        },
+        displayEditModal(){
+          return (this.showEditModal==true) ? 'u-show' : ''
+        },
+        displayDeleteModal(){
+          return (this.showDeleteModal==true) ? 'u-show' : ''
+        }
+      },
+      methods: {
+        toggleModal(modal){
+          if (modal ==='add'){
+            this.showAddModal=!this.showAddModal
+          }else if(modal==='edit'){
+            this.showEditModal=!this.showEditModal
+          }else if(modal==='delete'){
+            this.showDeleteModal=!this.showDeleteModal
+          }
+        },
+        setMessages(res){
+          if (res.data.error==1) {
+            this.errorMessage = res.data.message
+          }else{
+            this.successMessage = res.data.message
+            this.getAllStudents()
+          }
 
+          setTimeout(() =>{
+            this.errorMessage = false
+            this.successMessage = false
+          }, 2000)
+        },
+        getAllStudents(){
+          axios.get('./api.php?action=read')
+            .then(res=>{
+              //console.log(res)
+              this.setMessages(res)
+              this.students = res.data.students
+            })
+        },
+        createStudent(e){
+          axios.post( './api.php?action=create', new FormData(e.target))
+            .then(res => {
+              this.toggleModal('add')
+              this.setMessages(res)
+            })
+        },
+        getStudent(action, student){
+          this.toggleModal(action)
+          this.activeStudent = student
+        },
+        updateStudent(e){
+          axios.post( './api.php?action=update', new FormData(e.target))
+            .then(res => {
+              this.toggleModal('edit')
+              this.setMessages(res)
+            })
+        },
+        deleteStudent(e){
+          axios.post( './api.php?action=delete', new FormData(e.target))
+            .then(res => {
+              this.toggleModal('delete')
+              this.setMessages(res)
+            })
+        }
+      }
+    })
   </script>
 </body>
 </html>
